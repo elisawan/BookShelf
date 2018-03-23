468c34ec25469066b0262d17aded769aad90f3bb
Elisa <elisa.zhimin.wan@gmail.com>
2018-03-23 17:46:36 +0100

    ADD showUserActivity, editUserActivity

---
+++ app/src/main/java/com/afec/bookshelf/ShowUser.java
@@ -0,0 +1,13 @@
+package com.afec.bookshelf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_user);
+    }
+}
