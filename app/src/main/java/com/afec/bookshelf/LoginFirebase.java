da58f30c9a09cf85e7ec051d001910ba1f6fca36
Andrea <andrea.grippi@yahoo.it>
2018-04-16 09:17:43 +0200

    Auth + Database
    Aggiunto layout login

---
+++ app/src/main/java/com/afec/bookshelf/LoginFirebase.java
@@ -0,0 +1,27 @@
+package com.afec.bookshelf;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginFirebase extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_ui)
+        mAuth = FirebaseAuth.getInstance();
+    }
+}
