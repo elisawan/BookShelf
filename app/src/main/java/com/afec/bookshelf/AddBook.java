68006cd245d4cb25fd6fd2355bb9811cef6badab
Andrea <andrea.grippi@yahoo.it>
2018-04-18 16:44:50 +0200

    HTTP request
    Aggiunta chiamata http per ottenere i dati in json del libro.

--- app/src/main/java/com/afec/bookshelf/AddBook.java
+++ app/src/main/java/com/afec/bookshelf/AddBook.java
@@ -2,11 +2,19 @@ package com.afec.bookshelf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.w3c.dom.Text;
 
@@ -16,6 +24,10 @@ public class AddBook extends AppCompatActivity {
     EditText ISBN_reader, edit_location;
     Button ISBN_scan_button, Locate_button, confirm_button ;
     TextView ISBN_show, book_title, book_author, status_bar, location_bar;
+
+    //Web Call
+
+    String url = "https://www.googleapis.com/books/v1/volumes?q=isbn:9788854181595";
 
 
     @Override
@@ -37,8 +49,33 @@ public class AddBook extends AppCompatActivity {
         location_bar = (TextView) findViewById(R.id.location_bar);
 
 
-
+        isbnHttpRequest();
 
 
     }
+
+
+
+    public void isbnHttpRequest(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                       Log.e("Response","Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+               Log.e("Response error", "That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
 }
