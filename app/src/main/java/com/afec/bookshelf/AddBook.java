fe976f29e4d22d3a90b8c0c73c55d76c953bd757
Elisa <elisa.zhimin.wan@gmail.com>
2018-04-18 16:49:54 +0200

    ADD: isbn returned to addBook and shown in ISBN_reader

--- app/src/main/java/com/afec/bookshelf/AddBook.java
+++ app/src/main/java/com/afec/bookshelf/AddBook.java
@@ -1,27 +1,44 @@
 package com.afec.bookshelf;
 
+import android.*;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
 
 import org.w3c.dom.Text;
 
-public class AddBook extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static java.lang.System.in;
+
+public class AddBook extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
     ImageButton ib;
     EditText ISBN_reader, edit_location;
     Button ISBN_scan_button, Locate_button, confirm_button ;
     TextView ISBN_show, book_title, book_author, status_bar, location_bar;
-
+    ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
+
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA},2);
+        }
 
         ib = (ImageButton) findViewById(R.id.ib);
         ISBN_reader = (EditText) findViewById(R.id.ISBN_reader);
@@ -37,8 +54,40 @@ public class AddBook extends AppCompatActivity {
         location_bar = (TextView) findViewById(R.id.location_bar);
 
 
+        Bundle b = getIntent().getExtras();
+        if(b != null) {
+            String isbn = b.getString("isbn", null);
+            if (isbn != null) {
+                ISBN_reader.setText(isbn);
+            }
+        }
 
 
+    }
 
+    public void isbn_scan(View v) {
+        scannerView = new ZXingScannerView(getApplicationContext());
+        setContentView(scannerView);
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(scannerView != null)
+            scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        String isbn = result.getText();
+        //scannerView.resumeCameraPreview(this);
+        scannerView.stopCamera();
+        Intent intent = new Intent(getApplicationContext(),AddBook.class);
+        Bundle b = new Bundle();
+        b.putString("isbn",isbn);
+        intent.putExtras(b);
+        startActivity(intent);
     }
 }
