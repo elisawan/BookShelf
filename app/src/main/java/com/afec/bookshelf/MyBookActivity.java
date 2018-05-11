eae7ccdec3263ed81ba7f72bf2af2176ac0e0729
Andrea <andrea.grippi@yahoo.it>
2018-05-11 14:55:17 +0200

    Aggiunti listener disponibilità libro/stato libro + Modifica coordinate

--- app/src/main/java/com/afec/bookshelf/MyBookActivity.java
+++ app/src/main/java/com/afec/bookshelf/MyBookActivity.java
@@ -4,7 +4,9 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,11 +15,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
@@ -27,6 +31,9 @@ import android.widget.Toast;
 
 import com.afec.bookshelf.Models.Book;
 import com.afec.bookshelf.Models.BookInstance;
+import com.afec.bookshelf.Models.MyLocation;
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -36,20 +43,25 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.Locale;
+
 public class MyBookActivity extends Fragment {
 
     private TextView tv_title, tv_author, tv_publisher, tv_ed_year, tv_isbn, L;
     private ImageView iv_book;
-    private Spinner S;
+    private Spinner spinner;
     private int status;
     private Button locButton, delButton;
     private CheckBox avCheckbox;
+    MyLocation myLocation;
     FirebaseDatabase db;
     FirebaseUser currentUser;
     DatabaseReference item;
     String loc;
     String instance;
     Location location;
+    DatabaseReference bookInstanceRef;
+
 
     @Nullable
     @Override
@@ -64,7 +76,7 @@ public class MyBookActivity extends Fragment {
         tv_publisher = (TextView) v.findViewById(R.id.book_publisher);
         tv_title = (TextView) v.findViewById(R.id.book_title);
         iv_book = (ImageView) v.findViewById(R.id.book_image);
-        S = (Spinner) v.findViewById(R.id.status_spinner);
+        spinner = (Spinner) v.findViewById(R.id.status_spinner);
         status = 0;
         locButton = (Button) v.findViewById(R.id.editLocationButton);
         delButton = (Button) v.findViewById(R.id.DeleteButton);
@@ -82,12 +94,12 @@ public class MyBookActivity extends Fragment {
             toBookList();
         }
 
-        String isbn = b.getString("isbn");
+        final String isbn = b.getString("isbn");
         instance= b.getString("instance");
 
         if (isbn != null && isbn.length()==13) {
             DatabaseReference bookRef = FirebaseDatabase.getInstance().getReference("books").child(isbn);
-            DatabaseReference bookInstanceRef= FirebaseDatabase.getInstance().getReference("book_instances").child(instance);
+            bookInstanceRef = FirebaseDatabase.getInstance().getReference("book_instances").child(instance);
 
             bookRef.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
@@ -118,7 +130,7 @@ public class MyBookActivity extends Fragment {
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     BookInstance bi = dataSnapshot.getValue(BookInstance.class);
                     status=bi.getStatus();
-                    S.setSelection(status);
+                    spinner.setSelection(status);
                     loc=bi.getLocation().toString();
                     L.setText(loc);
                     avCheckbox.setChecked(bi.getAvailability());
@@ -182,6 +194,8 @@ public class MyBookActivity extends Fragment {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        Log.e("EDIT schiacciato", "wow");
+
                         if(ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
                             ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION},1);
                         }
@@ -190,9 +204,95 @@ public class MyBookActivity extends Fragment {
 
                         item = db.getReference("users").child(currentUser.getUid()).child("myBooks").child(instance).child("location");
                         item.setValue(location);
+
+                        getAddress();
+
+                        //Modifica Geofire di book instance
+                        DatabaseReference locRef = db.getReference("geofire");
+                        GeoFire geoFire = new GeoFire(locRef);
+                        geoFire.setLocation(instance, new GeoLocation(myLocation.getLatitude(), myLocation.getLongitude()), new GeoFire.CompletionListener() {
+                            @Override
+                            public void onComplete(String key, DatabaseError error) {
+                                if (error != null) {
+                                    Toast.makeText(getActivity(),"There was an error saving the location to GeoFire: " + error,Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getActivity(),"Location saved on server successfully!",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
                     }
                 }
         );
+
+        avCheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                builder.setMessage("Modificare la disponibilità del libro?")
+                        .setTitle("Modifica disponibilità");
+
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        if(avCheckbox.isChecked()){
+                            bookInstanceRef.child("availability").setValue(true);
+                        }else{
+                            bookInstanceRef.child("availability").setValue(false);
+                        }
+                    }
+                });
+                builder.setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+
+
+                AlertDialog dialog = builder.create();
+
+                dialog.show();
+
+
+            }
+
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, final int i, long l) {
+                if(i!=0){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                    builder.setMessage("Modificare lo stato del libro?")
+                            .setTitle("Modifica stato");
+
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked OK button
+                            bookInstanceRef.child("status").setValue(i);
+                        }
+                    });
+                    builder.setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+
+
+                    AlertDialog dialog = builder.create();
+
+                    dialog.show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         return v;
     }
@@ -214,6 +314,51 @@ public class MyBookActivity extends Fragment {
         menu.setGroupVisible(R.id.showProfileMenu,false);
     }
 
+
+    public void getAddress(){
+
+
+        if(ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+        LocationManager lm= (LocationManager) getActivity().getSystemService(getContext().LOCATION_SERVICE);
+
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+
+        myLocation= new MyLocation();
+
+        location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if(location==null) {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0, this.mLocationListener);
+            location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        else{
+            myLocation.setLatitude(location.getLatitude());
+            myLocation.setLongitude(location.getLongitude());
+        }
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            //your code here
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
 }
 
