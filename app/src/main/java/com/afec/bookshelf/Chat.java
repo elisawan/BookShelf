197f6107dda7b87c2c2f33b8b4e16dbe3f6a89da
Andrea <andrea.grippi@yahoo.it>
2018-05-14 10:46:15 +0200

    Inizio Chat
    activity_chat -> contenitore per chat
    ...message_layout -> layout messaggi utenti

---
+++ app/src/main/java/com/afec/bookshelf/Chat.java
@@ -0,0 +1,12 @@
+package com.afec.bookshelf;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Chat extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
