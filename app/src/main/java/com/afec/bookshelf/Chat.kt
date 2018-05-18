d2091ba3aaac937915239b416459a26daff50f53
Andrea <andrea.grippi@yahoo.it>
2018-05-18 14:01:29 +0200

    Chat -> push message

--- app/src/main/java/com/afec/bookshelf/Chat.kt
+++ app/src/main/java/com/afec/bookshelf/Chat.kt
@@ -3,6 +3,7 @@ package com.afec.bookshelf
 import android.app.Activity
 import android.app.Fragment
 import android.os.Bundle
+import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,9 @@ import android.widget.Toast
 import com.afec.bookshelf.Models.ChatMessage
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import org.w3c.dom.Text
+import java.sql.Time
 
 
 class Chat : Activity() {
@@ -21,12 +24,13 @@ class Chat : Activity() {
     lateinit var message_written:TextView
     lateinit var message:ChatMessage
     lateinit var user:FirebaseUser
-
+    lateinit var chatID:String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
 
         config()
     }
@@ -38,6 +42,13 @@ class Chat : Activity() {
         var fbAuth = FirebaseAuth.getInstance()
         user = fbAuth.currentUser!!
 
+
+        var fbRef = FirebaseDatabase.getInstance()
+                .reference
+                .child("chat/")
+                .child("1234567890")
+
+
         button_send = findViewById(R.id.button_chatbox_send)
         message_written = findViewById(R.id.edittext_chatbox)
 
@@ -47,7 +58,8 @@ class Chat : Activity() {
         button_send.setOnClickListener { view ->
             Toast.makeText(baseContext, "Write your message here", Toast.LENGTH_LONG).show()
             var messaggio = message_written.text.toString()
-
+            message = ChatMessage(messaggio, user.uid, 13213481263415)
+            fbRef.push()
 
         }
     }
