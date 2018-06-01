9743f7129855b46f9be9888021c5de4e56532e2c
Andrea <andrea.grippi@yahoo.it>
2018-06-01 12:32:47 +0200

    Update Review page.java
    manca inserimento liste nell'adapter

--- app/src/main/java/com/afec/bookshelf/ReviewPage.java
+++ app/src/main/java/com/afec/bookshelf/ReviewPage.java
@@ -1,43 +1,181 @@
 package com.afec.bookshelf;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TabHost;
+
+import com.afec.bookshelf.Models.Review;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Andrea on 29/05/2018.
  */
 
-public class ReviewPage extends AppCompatActivity {
-    TabHost tabHost;
+public class ReviewPage extends Fragment {
+
+
+
+    private FirebaseUser user;
+    String userUid;
+    FirebaseDatabase database;
+    DatabaseReference recRef, myRef;
+    List<Review> myReviews;
+    List<Review> receivedReviews;
+
+    private static final String TAG = "ReviewPage activity";
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reviews);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_search_books, container, false);
 
-        TabHost host = (TabHost)findViewById(R.id.review_tabs);
-        host.setup();
 
-        //Tab 1
-        TabHost.TabSpec spec = host.newTabSpec("Tab One");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Pending");
-        host.addTab(spec);
 
-        //Tab 2
-        spec = host.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("Written");
-        host.addTab(spec);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getActivity().getSupportFragmentManager());
 
-        //Tab 3
-        spec = host.newTabSpec("Tab Three");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("Received");
-        host.addTab(spec);
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) v.findViewById(R.id.rev_container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) v.findViewById(R.id.rev_tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        return v;
+    }
+
+    private void setupViewPager(ViewPager mViewPager){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getActivity().getSupportFragmentManager());
+
+        retrieveMyReviews();
+
+        retrieveReceivedReviews();
+
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        @Override
+        public String getPageTitle(int position){
+            return mFragmentTitleList.get(position);
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
     }
 
 
+    private void retrieveMyReviews(){
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userUid = user.getUid();
+
+        myRef = database.getReference("users/"+userUid).child("myReviews");
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void retrieveReceivedReviews(){
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userUid = user.getUid();
+
+        recRef = database.getReference("users/" + userUid).child("receivedReviews");
+        recRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Review review = dataSnapshot.getValue(Review.class);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 
 
 }
